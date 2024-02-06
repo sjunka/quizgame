5ef460703a79fdc1bc3f104b9c09151c63dbf4a3
Your Name <you@example.com>
2024-02-06 11:31:50 +0100

    feat: added request

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './LoginScreen';
-import ScoreboardScreen from './ScoreboardScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import ScoreboardScreen from './src/screens/ScoreboardScreen';
+
+import useFetchData from './src/screens/hooks/useFetchData';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const URL = 'https://gbfs.nextbike.net/maps/gbfs/v2/nextbike_na/gbfs.json';
+
+  const { data, loading, error } = useFetchData(URL);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Landing" component={LandingScreen} />
+        <Stack.Screen initialRouteName="Landing" name="Landing" component={LandingScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Scoreboard" component={ScoreboardScreen} />
       </Stack.Navigator>
@@ -19,13 +25,14 @@ export default function App() {
   );
 }
 
+
 function LandingScreen() {
   return (
     <View style={styles.container}>
       <Text>Welcome to the Landing Page!</Text>
       <Button
         title="Go to Login"
-        onPress={() => navigation.navigate('Login')}
+        onPress={() => navigation.navigate('Login', { data })}
       />
 
     </View>
