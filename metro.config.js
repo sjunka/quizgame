d63b0d68bc94b655560929af56de21a5fbcc4c16
Your Name <you@example.com>
2024-02-06 10:53:26 +0100

    commit message

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+getDefaultConfig.resolver.assetExts.push('cjs');
+
+module.exports = config;
